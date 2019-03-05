9b7b7b31210e5077bbdec3cef8cc7ed2cc452172
Lucas Merat <lucas.merat@gmail.com>
2019-03-05 14:28:42 -0600

    Fixed server setup for static files

--- public/server.js
+++ public/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use('/assets', express.static('assets'));
+app.use(express.static(__dirname + "/../public"));
 
 app.get('/', function(req,res){
     res.render('index')
